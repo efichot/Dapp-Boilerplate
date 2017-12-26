ac31ca3401b35f52505cda49b68da533c6a9ec3e
Etienne Fichot <fichotetienne@gmail.com>
2017-12-26 16:23:46 +0100

    starting from create-react-app

---
+++ README.md
@@ -0,0 +1,23 @@
+# Boilerplate for app && Dapp
+### Synapsis
+Full speed Ahead for internet 3.0 and Blockchain Interopability.
+This project was bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app).
+```
+git clone https://github.com/efichot/Dapp-Boilerplate
+
+yarn
+
+```
+**Distributed Autonomous Management System**
+The Boilerplate aims to solve the challenge of starting a dapp or a app where you need to manage, organize and facilitate a large distributed network of people.
+
+**This project will grow over time**
+As of right now, the primary objective is to refine the user interface
+
+## How it's Built
+The Frontend is primary built in React, Redux and Redux Saga.
+The Backend is built primary with Firebase.
+
+1. Atomic Design Philosophy
+2. Component/Container Separation
+3. Functional State Management
---
+++ src/index.js
@@ -0,0 +1,26 @@
+/*----------------------External Dependencies--------------------------*/
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { AppContainer } from 'react-hot-loader';
+
+/*----------------------Internal Dependencies--------------------------*/
+import Root from "interface";
+import { configureStore, history } from "store/configuration";
+import registerServiceWorker from './registerServiceWorker';
+
+/*----------------------Initialize Dependencies--------------------------*/
+const store = configureStore();
+
+/*----------------------Module Package--------------------------*/
+const rootElement = document.getElementById('root');
+ReactDOM.render(<AppContainer><Root store={ store } history={ history } /></AppContainer>, rootElement);
+
+if (module.hot) {
+    module.hot.accept('interface', () => {
+        const nextRoot = require('interface');
+        ReactDOM.render(<AppContainer><NextRoot store={ store } history={ history } /></AppContainer>, rootElement );
+    })
+}
+
+/*----------------------Service Worker--------------------------*/
+registerServiceWorker();

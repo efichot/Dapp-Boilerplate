9b30a54a3d78465bfa1f6609f658455c1bff5eaa
Etienne Fichot <fichotetienne@gmail.com>
2017-12-29 11:27:55 +0100

    link for cloudfunction with firebase update && FIREBASE_CONFIG updated

--- src/settings/Firebase/index.js
+++ src/settings/Firebase/index.js
@@ -1,9 +1,11 @@
 module.exports = {
-    apiKey: "AIzaSyAl-ivduMFdqWnoGiQop9t7cv_Ffyn2-Mk",
-    authDomain: "yser-c2017.firebaseapp.com",
-    databaseURL: "https://yser-c2017.firebaseio.com",
-    projectId: "yser-c2017",
-    storageBucket: "yser-c2017.appspot.com",
-    messagingSenderId: "953272369148"
+    FIREBASE_CONFIG: {
+        apiKey: "AIzaSyAl-ivduMFdqWnoGiQop9t7cv_Ffyn2-Mk",
+        authDomain: "yser-c2017.firebaseapp.com",
+        databaseURL: "https://yser-c2017.firebaseio.com",
+        projectId: "yser-c2017",
+        storageBucket: "yser-c2017.appspot.com",
+        messagingSenderId: "953272369148"
+    },
 };
     

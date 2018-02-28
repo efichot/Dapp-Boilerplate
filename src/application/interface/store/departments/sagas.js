17860852abaa51288bcd1563e2fb035043e0ce3b
Etienne FICHOT <fichotetienne@gmail.com>
2018-02-28 11:23:17 +0100

    mv uport into departements

--- src/application/interface/store/departments/sagas.js
+++ src/application/interface/store/departments/sagas.js
@@ -1,12 +1,11 @@
 import { all } from 'redux-saga/effects';
 
 import notifications from './notifications/sagas'
+import uport from './uport/sagas'
 
-// Blockchain Integrations
-import symbiosis from 'assimilation/symbiosis/sagas'
 export default function* rootSaga() {
   yield all([
-    ...symbiosis,
+    uport(),
     notifications()
   ]);
 }
